b6e3f5656ceca61479a43b53dbf426853fef045b
jasonskorup <jskorup@mac.com>
2018-01-12 16:04:23 -0800

    test ssh fix

---
+++ mod1_lab2.py
@@ -0,0 +1,9 @@
+user_name = input("Enter your name: ")
+
+user_color = input("Enter your favorite color: ")
+
+my_name = "Jason"
+
+my_color = "blue"
+
+print("Sorry", user_name, ", my name is ", my_name, "and ", my_color, "is a better color than", user_color, ".")
