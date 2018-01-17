670f6d3cfecc8911700a2520adb630b48c15a15c
jskorup <jskorup@tableau.com>
2018-01-16 17:13:22 -0800

    Homework 1

---
+++ hw1.py
@@ -0,0 +1,9 @@
+print("What would you like on your pizza?")
+
+cheese = input("What type of cheese? ")
+
+meat = input("What type of meat? ")
+
+veggie = input("What type of vegetable? ")
+
+print("One pizza with", cheese, ",", meat, ", and", veggie, "coming right up!")
