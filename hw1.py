3ab2958a9383953a924983ebc6e752ca76f8200d
jskorup <jskorup@tableau.com>
2018-01-17 14:20:21 -0800

    Homework 1

--- hw1.py
+++ hw1.py
@@ -1,9 +1,18 @@
-print("What would you like on your pizza?")
+"""
+This is a script that decides what to get on a pizza
+"""
 
+# Let the user know what we are asking about
+print("What would you like on your pizza:")
+# User inputs cheese preference
 cheese = input("What type of cheese? ")
-
+# User inputs meat preference
 meat = input("What type of meat? ")
-
+# User inputs veggie preference
 veggie = input("What type of vegetable? ")
 
-print("One pizza with " + cheese + ", " + meat + ", and " + veggie + " coming right up!")
+def pizza():
+    # Print concatenated response/result
+    print("One pizza with " + cheese + ", " + meat + ", and " + veggie + " coming right up!")
+
+pizza()
