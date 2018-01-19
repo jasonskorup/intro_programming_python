1a62968aee8552da2ccccc1c9aacd30245a3469a
jasonskorup <jskorup@mac.com>
2018-01-18 20:56:10 -0800

    Homework 2

---
+++ hw2.py
@@ -0,0 +1,10 @@
+'''
+Gather the following user input and store each item as a variable:
+Purchaser name
+Purchaser address
+Purchaser phone number (entered as 503-555-1211)
+Car Make/Model
+Purchase Price
+'''
+
+name = input("What is your name? ")
