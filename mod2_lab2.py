161e70c4597aee837ce10734aba49c654e46f5b9
jasonskorup <jskorup@mac.com>
2018-01-18 20:36:34 -0800

    Module 2, Lab 2

---
+++ mod2_lab2.py
@@ -0,0 +1,12 @@
+import random
+
+test_num = random.randint(50, 100)
+
+if test_num % 3 == 0 and test_num % 5 == 0:
+    print("fizzbuzz")
+
+elif test_num % 3 == 0:
+    print("fizz")
+
+elif test_num % 5 == 0:
+    print("buzz")
